e215050686971e8f9b7e62d09ad1e6ee1c1c3436
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-10-27 03:08:21 -0300

    Implementando syscalls
    Implementando syscalls de read, write, open, close e exit

--- src/system.c
+++ src/system.c
@@ -1,13 +1,76 @@
 #include "simulator.h"
+#include "log.h"
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+
+#define STDIN  0
+#define STDOUT 1
+#define STDERR 2
+
+
+void execute_read_syscall(){
+    int fd = registers[10];
+    int size = registers[12];
+    char *ptr = (char *)(memory + registers[11]);
+
+    registers[10] = read(fd, ptr, size);
+}
+
+void execute_write_syscall(){
+    int fd = registers[10];
+    int size = registers[12];
+
+    char *ptr = (char *)(memory + registers[11]);
+
+    registers[10] = write(fd, ptr, size);
+}
+
+void execute_ecall(){
+    switch(registers[17]){ // Get ecall code
+        case 57: //close
+            close(registers[10]);
+            break;
+        case 63: //read
+            execute_read_syscall();
+            break;
+        case 64: //write
+            execute_write_syscall();
+            break;
+        case 93: //exit
+            end = clock();
+            print_info("Program exited with code %d\n", registers[10]);
+            print_info("Instructions executed: %d\n", inst_count);
+    
+            unsigned int cycles = end - start;
+            double elapsed = (double)(end - start)/CLOCKS_PER_SEC;
+
+            print_info("Time measured: %.9f seconds.\n", elapsed);
+            print_info("Cycles measured: %d\n", cycles);
+            exit(registers[10]);
+            break;
+        case 1024: //open
+            registers[10] = open((char *)(memory + registers[10]), 
+                registers[11], registers[12]);
+            break;
+
+        default:
+            print_error("Invalid syscall code %d\n", registers[17]);
+            exit(1);
+            break;
+    }
+}
 
 
 int execute_system_instruction(InstData * instruction){
-    switch (instruction->funct3)
+    mode = MACHINE;
+
+    switch (instruction->imm)
     {
     case 0: // ECALL
-        printf("System call\n");
+        execute_ecall();
         break;
     
     case 1: // EBREAK
